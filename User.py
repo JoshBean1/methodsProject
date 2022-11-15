b066ff29e8d6a2b9e61e69ce3342da7b829b4b36
Joshua Bean <jab382@protonmail.com>
2022-11-15 16:11:37 -0600

    started user class, added main.py and users.csv files

---
+++ User.py
@@ -0,0 +1,45 @@
+import csv
+
+class User:
+    def __init__(self, ID, username, password, name, payment_info, email):
+        self.ID = ID
+        self.username = username
+        self.password = password
+        self.name = name
+        self.payment_info = payment_info
+        self.email = email
+
+    def login(self, username, password):
+        with open('users.csv', 'r') as users_file:
+            users_reader = csv.reader(users_file, delimiter=',')
+            for row in users_reader:
+                if row[1] == username:
+                    if row[2] == password:
+                        return True
+        return False
+
+    def change_name(self, name):
+        self.name = name
+
+    def change_pass(self, password):
+        self.password = password
+
+    def change_email(self, email):
+        self.email = email
+
+    def user_cart(self, itemID, cartID):
+        pass
+
+    def _update():  # write class to csv file
+        lines = []  # store csv in memory
+        current_user = [self.ID, self.username, self.password, self.name, self.payment_info, self.email]
+
+        with open('users.csv', 'r') as users_file:
+            users_read = csv.reader(users_file, delimiter=',')
+            for row in users_read:
+                if self.ID != row[0]:  # read all users into list except for current user
+                    lines.append(row)
+        with open('users.csv', 'w') as users_file:  # overwrite users.csv file with updated data
+            users_write = csv.writer(users_file, delimiter=',')
+            users_write.writerows(lines)  # old unchanged users
+            users_write.writerow(current_user)  # new user
