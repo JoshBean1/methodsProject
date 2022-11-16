f8e8fad8ae2dabfc754aae0165044359d575f5ca
Zchandler288u0ufuq <72233122+Zchandler288u0ufuq@users.noreply.github.com>
2022-11-16 12:45:37 -0600

    Add files via upload

---
+++ movieandbook.py
@@ -0,0 +1,26 @@
+class Book():
+    def _init_(self, ISBN, name, count, price):
+        self.ISBN = ISBN
+        self.name = name
+        self.count = count
+        self.price = price
+
+    def add_book_to_cart(self, ISBN, name, count, price):
+         return self.ISBN
+         return self.name
+         return self.count
+         return self.price
+
+
+class Movie():
+    def _init_(self, ISBN, name, count, price):
+        self.ISBN = ISBN
+        self.name = name
+        self.count = count
+        self.price = price
+
+    def add_movie_to_cart(self, ISBN, name, count, price):
+        return self.ISBN
+        return self.name
+        return self.count
+        return self.price
