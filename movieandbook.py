7612c10272bf84d3af1d1698baa2c551dbbf545f
Zchandler288u0ufuq <72233122+Zchandler288u0ufuq@users.noreply.github.com>
2022-11-21 16:53:03 -0600

    Update movieandbook.py

--- movieandbook.py
+++ movieandbook.py
@@ -1,26 +1,40 @@
 class Book():
-    def _init_(self, ISBN, name, count, price):
-        self.ISBN = ISBN
+    def _init_(self, ID, name, count, price):
+        self.ID = ID
         self.name = name
         self.count = count
         self.price = price
 
     def add_book_to_cart(self, ISBN, name, count, price):
-         return self.ISBN
+         return self.ID
          return self.name
          return self.count
          return self.price
 
 
 class Movie():
-    def _init_(self, ISBN, name, count, price):
-        self.ISBN = ISBN
+    def _init_(self, ID, name, count, price):
+        self.ID = ID
         self.name = name
         self.count = count
         self.price = price
 
     def add_movie_to_cart(self, ISBN, name, count, price):
-        return self.ISBN
+        return self.ID
         return self.name
         return self.count
         return self.price
+
+
+f = open("myCart.txt", "a")
+
+ID = input("Enter an ID: ")
+f.write("{" + ID + ",  ")
+name = input("Enter an name: ")
+f.write(name + ",   ")
+count = input("Enter a count: ")
+f.write(count + ",   ")
+price = input("Enter a price: ")
+f.write(price + "}" + "\n")
+
+f.close()
