b6d62464465385c16bc41e2b6f99ca5a09646175
Micheal-Mac <117315792+Micheal-Mac@users.noreply.github.com>
2022-11-23 12:04:58 -0600

    Update Inventory.py

--- Inventory.py
+++ Inventory.py
@@ -17,4 +17,22 @@ class Inventory():
         return self.itemPrice + self.count
         
     def veiew_all_items():
-        
+        import os
+        files = [] #['movies' 'books']
+        merged_data = ""
+
+        while True:
+            f_name = input{"Enter the file name: "}
+            files.append(f_name)
+            ans = input("Want to view another file?(y/n): ").lower()
+            if ans!='y':
+                break
+        for file in files:
+            filename = file + '.txt'
+            if os.path.isfile(filename):
+                f = open(filename, mode='r', encoding='utf-8')
+                merged_data = merged_data + f.read()
+                f.close() 
+        f.write(merged_data)
+        print("See all items in new list")
+
