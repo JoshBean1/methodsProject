31428d14db566451752e54454eb33cb224810cd5
Joshua Bean <jab382@protonmail.com>
2022-11-28 17:41:10 -0600

    pytest

--- hw5/functions_test.py
+++ hw5/functions_test.py
@@ -1,5 +1,5 @@
 import pytest
-import functions
+from functions import *
 
 ### TODO:
 '''
@@ -12,3 +12,28 @@ sq()
 greetUser()
 displayItem()
 '''
+
+########   openFile   ########
+def test_openFile():
+    assert openFile("testing.txt") == None
+
+def test_openFile_int():
+    assert openFile(123) == None
+
+def test_openFile_wrong_name():
+    assert openFile("test.txt") == None
+
+##############################
+
+########   numbers   #########
+def test_numbers():
+    assert numbers(8,4) == 2
+
+def test_numbers_float():
+    assert numbers(2.2, 1.1) == 2
+
+def test_numbers_datatype():
+    assert numbers('abc', [1,2,3]) == None
+
+def test_numbers_both():
+    assert numbers(2.2, 2) == 1.1
