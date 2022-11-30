38418c648b55e7101fbd54e6a92ecb9856b2c06d
Joshua Bean <jab382@protonmail.com>
2022-11-30 10:22:37 -0600

    update file names

--- Inventory.py
+++ Inventory.py
@@ -9,16 +9,16 @@ class Inventory:
     def dec_count(self):
         self.count -= 1
         
-    def __repr__(self): #veiw all items
+    def __repr__(self): #view all items
         return str(self.book_list) + str(self.movie_list)
         
     def veiw_books(self):
-        f = open("book.txt")
+        f = open("books.txt")
         book_content = f.read()
         print(book_content)
         f.close()
     def veiw_movies(self):
-        f =open("movie.txt")
+        f =open("movies.txt")
         movie_content = f.read()
         print(movie_content)
         f.close()
