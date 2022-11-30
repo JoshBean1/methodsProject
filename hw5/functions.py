fdacf4971b909f9ff0d4e82a1b4c527bab85bab2
Micheal-Mac <117315792+Micheal-Mac@users.noreply.github.com>
2022-11-30 15:19:31 -0600

    Update functions.py

--- hw5/functions.py
+++ hw5/functions.py
@@ -43,12 +43,17 @@ def isPalindrome(temp):
 ## has input to receive two numbers
 ## divides the two, then outputs the result
 def divide():
-    num1 = int(input("Enter a number: "))
-    num2 = int(input("Enter another number: "))
+    try:
+        num1 = int(input("Enter a number: "))
+        num2 = int(input("Enter another number: "))
 
-    div = num1 / num2
+        div = num1 / num2
 
-    print("Your numbers divided is:", div)
+        print("Your numbers divided is:", div)
+    except ZeroDivisionError:
+        print("\nYou tried to divide by zero.")
+    except:
+        print("\nAn error occurred")
 
 
 ## returns the squareroot of a particular number
